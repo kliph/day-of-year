c7ca4386a0eb2a214c54bf1c62b13bb0fa7e7759
Cliff Rodgers <kliph@users.noreply.github.com>
2018-03-28 19:53:14 -0400

    Feature complete XD

--- src/day_of_year/core.cljs
+++ src/day_of_year/core.cljs
@@ -1,11 +1,11 @@
 (ns day-of-year.core
   (:require [reagent.core :as r]
-            [goog.dom]))
+            [goog.dom]
+            date-fns))
 
 (def by-id goog.dom.getElement)
 
 (defn app-container []
-  [:h1 {}
-   "Hello World"])
+  [:h1 {} (date-fns/format (js/Date.) "DDDD")])
 
 (r/render-component [app-container] (by-id "app"))
